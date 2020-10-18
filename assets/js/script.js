57bb75c973dc90e4a16a3ffeae5ec4dc91131c42
krunal r prajapati <ghanshyamskb@gmail.com>
2020-10-18 13:48:53 -0700

    ui bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 $(function() {
-    $(".hamburgar").on("click", function(){
-        $(".bottomHeader").addClass("open");
+    $(".hamburgar").on("click", function() {
+        $(".nav").toggleClass("open");
+        $(this).toggleClass("open");
     });
-    $(".closeNav").on("click", function(){
-        $(".bottomHeader").removeClass("open");
-    });
+    // $(".closeNav").on("click", function() {
+    //     $(".bottomHeader").removeClass("open");
+    // });
     var bannerHeight = $(window).height() - $("header").height();
     $(".banner_blk").height(bannerHeight);
-  
-    
+    $('.bannerSlider').slick({
+        infinite: true,
+        arrows: false
+    });
+
 });
 
-$( window ).resize(function() {
-});
-
- 
+$(window).resize(function() {});
